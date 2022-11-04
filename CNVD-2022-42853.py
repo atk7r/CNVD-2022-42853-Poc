4f4a32f00e5f8741594f7bf7242fefa6ccd8331c
atk7r <94058716+atk7r@users.noreply.github.com>
2022-11-05 06:22:08 +0900

    Update CNVD-2022-42853.py

--- CNVD-2022-42853.py
+++ CNVD-2022-42853.py
@@ -53,7 +53,7 @@ class attack():
                 print("\033[31m[x]\033[0m" + "\033[31m{}\033[0m".format(httpline) + "\033[31m error \033[0m")
 
 if (__name__ == "__main__"):
-    parser = argparse.ArgumentParser(description="CNVD-2022-42853 Poc by ccj")
+    parser = argparse.ArgumentParser(description="CNVD-2022-42853 Poc by atk7r")
     parser.add_argument(
         '-ip', '--ipaddress', type=str, metavar="",
         help='Please input url to scan.'
